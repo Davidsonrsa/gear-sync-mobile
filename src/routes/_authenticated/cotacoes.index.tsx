466e6f047fc34662a1040f35641e5af0d9eb385a
Davidsonrsa <davidsonrsa@gmail.com>
2026-08-28 15:55:52 -0300

    Update cotacoes.index.tsx

--- src/routes/_authenticated/cotacoes.index.tsx
+++ src/routes/_authenticated/cotacoes.index.tsx
@@ -19,8 +19,7 @@ import {
 } from "lucide-react";
 import { toast } from "sonner";
 
-export const Route = createFileRoute("/_authenticated/cotacoes")({
-  component: CotacoesPage,
+export const Route = createFileRoute("/_authenticated/cotacoes/")({
 });
 
 type Cotacao = {
